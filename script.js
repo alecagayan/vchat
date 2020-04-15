8806f59d5c91310f622d3d2d9028000558b593fe
oopsie1412 <alecagayan24@gmail.com>
2020-04-15 13:24:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
+const drone = new ScaleDrone('cU9z7ev26H7O3P2f');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
